c22e7c7b49f9d06a6240901da9b276a9a46a11cf
RonnyCabrera <ronny-38@hotmail.com>
2018-05-16 15:41:16 -0500

    Creacion Entidad Usuario

--- project/src/app.module.ts
+++ project/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UsuarioEntity} from "./usuario/usuario.entity";
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: 'root',
+          database: 'web',
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          synchronize: true,
+      }),
+      TypeOrmModule.forFeature([UsuarioEntity])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
