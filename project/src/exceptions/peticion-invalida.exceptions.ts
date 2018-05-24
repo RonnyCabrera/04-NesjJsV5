b38510a84bae32dc0a54cc78730a68ec4aff0a3a
RonnyCabrera <ronny-38@hotmail.com>
2018-05-23 19:25:28 -0500

    Peticion Invalida - Exceptions

---
+++ project/src/exceptions/peticion-invalida.exceptions.ts
@@ -0,0 +1,18 @@
+import {HttpException, HttpStatus} from "@nestjs/common";
+
+
+export class PeticionInvalidaExceptions extends HttpException {
+    constructor (private _mensaje,
+                 private _detalle,
+                 private _nivelError) {
+        super(
+            {
+                mensaje:_mensaje,
+                detalle:_detalle,
+                nivelError:_nivelError,
+                status: HttpStatus.BAD_REQUEST //Status Code
+            },
+            HttpStatus.BAD_REQUEST
+        );
+    }
+}
